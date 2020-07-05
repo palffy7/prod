839a55b2d9154ce744c56bd54defc3bbc2f9ae61
palffy7 <ralph.amhof@hotmail.com>
2020-07-05 22:32:59 +0200

    Updates

---
+++ precache-manifest.ae663a4039a64936198e5d1e438ff4d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c99c6d1325d088aeca6a5b669a428ba",
+    "url": "https://palffy7.github.io/prod/index.html"
+  },
+  {
+    "revision": "5e6e0d4b4681612ad3b1",
+    "url": "https://palffy7.github.io/prod/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "403493bde431b0361074",
+    "url": "https://palffy7.github.io/prod/static/js/2.d460aab8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://palffy7.github.io/prod/static/js/2.d460aab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6e0d4b4681612ad3b1",
+    "url": "https://palffy7.github.io/prod/static/js/main.c83a152b.chunk.js"
+  },
+  {
+    "revision": "306cf3f139d9f3bfde33",
+    "url": "https://palffy7.github.io/prod/static/js/runtime-main.4c5ebc59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://palffy7.github.io/prod/static/media/logo.5d5d9eef.svg"
+  }
+]);
